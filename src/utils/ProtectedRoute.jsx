1b5642f8d93a069600f4f19be97dbe3fcf0e30ec
korapana dipak <129277793+dipakpatel123@users.noreply.github.com>
2025-03-13 15:01:04 +0530

    add commnet

--- src/utils/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
+// ProtectedRoute is a component that checks i  f the user is logged in
+// If the user is not logged in, it redirects to the login page
+// If the user is logged in, it renders the children components
 const ProtectedRoute = ({ children }) => {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
